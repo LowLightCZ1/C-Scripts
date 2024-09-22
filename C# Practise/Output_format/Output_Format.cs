69f1a3bd42726a943da64b1be6ae7352b5fef8b4
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2024-09-22 16:36:36 +0200

    Update Output_Format.cs

--- C# Practise/Output_format/Output_Format.cs
+++ C# Practise/Output_format/Output_Format.cs
@@ -9,20 +9,23 @@ namespace OutputProgram
             Console.Write("Hey!"); // Write in one line
             Console.WriteLine("Hello!"); // "String literal" + Make new whole line
 
-            Console.WriteLine("\tBroCode");
+            Console.WriteLine("\tBroCode"); // Example of Escape Sequences
+
+            Console.ReadKey(); // Stop program only after you press key in terminal
         }
     }   
 }
+
 /* Escape Sequences
              * \a = Bell(alert)
              * \b = Backspace
-             * \f
-             * \n
-             * \r
-             * \t
-             * \v
-             * \'
-             * \"
-             * \\
-             * \?
+             * \f = Form feed
+             * \n = New line
+             * \r = Carriage return
+             * \t = Horizaontal tab
+             * \v = Vertical tab
+             * \' = Single quotation mark
+             * \" = Double quotation mark
+             * \\ = Backslash
+             * \? = Literal quotation mark
              */
