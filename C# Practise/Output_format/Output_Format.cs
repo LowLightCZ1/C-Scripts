9ed8d7f49a31346913ff8c3f537b6283654d4bf6
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2024-09-22 16:27:57 +0200

    Update Output_Format.cs

--- C# Practise/Output_format/Output_Format.cs
+++ C# Practise/Output_format/Output_Format.cs
@@ -6,8 +6,23 @@ namespace OutputProgram
     {
         static void Main(string[] args)
         {
-            Console.Write("Hey!");
-            Console.WriteLine("Hello!"); // String literal
+            Console.Write("Hey!"); // Write in one line
+            Console.WriteLine("Hello!"); // "String literal" + Make new whole line
+
+            Console.WriteLine("\tBroCode");
         }
     }   
 }
+/* Escape Sequences
+             * \a = Bell(alert)
+             * \b = Backspace
+             * \f
+             * \n
+             * \r
+             * \t
+             * \v
+             * \'
+             * \"
+             * \\
+             * \?
+             */
