e1d358a48a9d3d2e16d74308931e93d2a641da77
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2024-09-22 15:53:42 +0200

    Create Output_Format.cs

---
+++ Output_Format.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Output_Format
+{
+     class Program
+    {
+        static void Main(string[] agrs)
+        {
+            Console.Write("Hey!");
+            Console.WriteLine("Hello!"); // String literal
+        }
+    }
+}
