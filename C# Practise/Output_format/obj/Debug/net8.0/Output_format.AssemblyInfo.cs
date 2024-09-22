1977297ae5484ed088c1910ddf26cc646a451733
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2024-09-22 16:36:51 +0200

    Update Output_format.AssemblyInfo.cs

--- C# Practise/Output_format/obj/Debug/net8.0/Output_format.AssemblyInfo.cs
+++ C# Practise/Output_format/obj/Debug/net8.0/Output_format.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Output_format")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c9ee160e0cf2081f0b7fddeed3c83056c85a3f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+283e86dfed0db6c1bff2506d1eebfbcc980ff4e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Output_format")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Output_format")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
