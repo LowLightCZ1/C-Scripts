6478a791e496de80bf31feb3fc6c426113643ffe
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2024-10-06 17:30:33 +0200

    Update Morse_code.cs

--- C# Practise/Morse Code/Morse_code.cs
+++ C# Practise/Morse Code/Morse_code.cs
@@ -5,10 +5,12 @@ namespace MyProgram
      class Program
     {
         static void Main(string[] agrs)
-
         {
-            Console.WriteLine("Fuck off C#"); // Place where you can write text
-            Console.Beep(); // Beep after a code is running
+            int dot = 600;
+            int line = 900;
+           
+            Console.Beep(1000, dot);
+            Console.Beep(1000, line); // Beep after a code is running
         }
     }
 }
