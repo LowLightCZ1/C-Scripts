c3b488d46935ad09d56cb391f671af0d831cd096
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2024-10-06 17:30:27 +0200

    Update Morse Code.AssemblyInfo.cs

--- C# Practise/Morse Code/obj/Debug/net8.0/Morse Code.AssemblyInfo.cs
+++ C# Practise/Morse Code/obj/Debug/net8.0/Morse Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Morse Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7cfcd8e44e897685fb14a768d03be9e0e16faa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70da12685f143ae8f194cbd15852e4fdc10e8089")]
 [assembly: System.Reflection.AssemblyProductAttribute("Morse Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Morse Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
