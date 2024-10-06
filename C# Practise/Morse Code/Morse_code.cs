2c3c491495bd65a92924fb1fbec6b51e1fccadc5
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2024-10-06 19:38:35 +0200

    Update Morse_code.cs

--- C# Practise/Morse Code/Morse_code.cs
+++ C# Practise/Morse Code/Morse_code.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace MyProgram
 {
@@ -6,11 +7,21 @@ namespace MyProgram
     {
         static void Main(string[] agrs)
         {
+            Dictionary<int, string> Morse = new();
+
+            Morse.Add(1, "Hello");
+            
+            
+            
             int dot = 600;
             int line = 900;
            
             Console.Beep(1000, dot);
             Console.Beep(1000, line); // Beep after a code is running
+
+            Console.WriteLine(Morse[1]);
+
+
         }
     }
 }
