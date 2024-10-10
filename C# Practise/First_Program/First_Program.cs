974425342a0a382de2f9c38440bf42d2216b026d
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2024-10-10 18:41:46 +0200

    Update First_Program.cs

--- C# Practise/First_Program/First_Program.cs
+++ C# Practise/First_Program/First_Program.cs
@@ -8,7 +8,7 @@ namespace MyProgram
 
         {
             Console.WriteLine("Fuck off C#"); // Place where you can write text
-            Console.Beep(); // Beep after a code is running
+            Console.Beep(); // Beep after a code is running 
         }
     }
 }
