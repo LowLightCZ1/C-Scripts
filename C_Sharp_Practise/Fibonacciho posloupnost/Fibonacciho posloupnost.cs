29fc3b16edbf7546998423d6cef496e64338623a
LowLightCZ1 <155197914+LowLightCZ1@users.noreply.github.com>
2024-11-24 18:52:44 +0100

    Update

--- C_Sharp_Practise/Fibonacciho posloupnost/Fibonacciho posloupnost.cs
+++ C_Sharp_Practise/Fibonacciho posloupnost/Fibonacciho posloupnost.cs
@@ -1,17 +1,39 @@
 ﻿using System;
-using System.Collections.Generic
-using System.Linq;
-using System.Text;
+using static System.Linq.Enumerable;
+
 
 namespace Posloupnost
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-
+            Console.WriteLine(Fib(4));
+            Console.WriteLine(FibRek(6));
         }
 
-        private static 
+        private static int FibRek(int n)
+        {   
+           return  (n < 2)? n : Fib(n - 1) + Fib(n - 2);
+        }
+
+        public static int Fib(int n) 
+        {
+            if(n == 0) {
+                return 0;
+            }
+           
+            int prev = 0;
+            int fib = 1;
+            
+            for (int i = 1; i < n; i++)
+            {
+                int sum = prev + fib;
+                prev = fib;
+                fib = sum;
+            }
+            return fib;
+        }
     }
+    
 }
